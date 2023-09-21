eeb58ef4ecc916055ba5578f8d12ab4b8195906a
Viishal-rajput07 <rajputanavishal7@gmail.com>
2023-09-21 15:06:21 +0530

    COMMIT

---
+++ 1fav_data_types_comparrion.js
@@ -0,0 +1,26 @@
+// DATATYPES COMPARRISON 
+
+// IN COMPARISON BY DEFAULT JAVASCRITPT FIRST CONVERT THE DATATYPES AND THEN GIVE THE RESULT
+// IN COMPARISON NULL ACTS LIKE 0
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null>=0)
+// console.log(null==0)
+// console.log(null===0)
+
+
+// HERE STRING 2 IS CONVERTED INTO NUMBER AND THEN COMPARED
+// console.log("2" >2)
+// console.log("2" <2)
+// console.log("2" == 2)
+// console.log("2" === 2)
+
+// SAME BEHAVIOUR IS SHOWN BY UNDEFINED BUT  WHEN WE COMPARE UNDEFINED == NULL IT SHOWS TRUE
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined>=0)
+// console.log(undefined==0)
+// console.log(undefined===0)
+// console.log(undefined == null)
+// console.log(0 <= null)
+
